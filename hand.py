b8d37e4deebad084d6577bbd58b468251010c864
Fabian Furger <furgerfabian@hotmail.com>
2018-02-18 10:28:16 +0100

    Some cleanup

--- hand.py
+++ hand.py
@@ -8,6 +8,7 @@ from round import Round
 
 
 class Hand():
+  HAND_SCORE_FACTOR = 2
 
   def __init__(self, players, cards, training_data, log):
     self._players = players
@@ -18,45 +19,47 @@ class Hand():
     self.log = log
     self._known_cards = np.zeros(36, dtype=int)
 
-    # shuffle
+    # shuffle and distribute cards
     self.cards = np.random.permutation(cards)
-    # distribute
     for i in range(len(self._players)):
       self._players[i].hand = self.cards[i*9:(i+1)*9]
 
 
   def play(self, dealer):
-    # choose trump
+    """Plays the hand by playing 9 rounds.
+
+    :dealer: (int) Index of the player that plays the first card.
+    """
+
+    # choose trump and set up cards accordingly
     trump = "obenabe"
-    [c.set_score(trump) for c in self.cards]
     self.log.info("Playing hand with trump: {}".format(trump))
+    [c.set_score(trump) for c in self.cards]
 
     training_data_team_1 = []
     training_data_team_2 = []
-    HAND_SCORE_FACTOR = 2
+
     for i in range(9):
       self.log.debug("---------- Round {} ----------".format(i+1))
       current_round = Round(self._players, self._known_cards, self.log,)
-      dealer, played_cards, score, states = current_round.play(dealer)
+      dealer, score, played_cards, states = current_round.play(dealer)
+
+      # update known cards - mark the cards that were played during this round
       for j in range(4):
         self._known_cards[played_cards[j].card_index] = j+1 # TODO: Check if this needs to be rotated and if some j should be an i
       self.log.debug("Known cards: {}".format(utils.format_cards(self._known_cards)))
-      if i < 8: # skip one-card decisions
-        if dealer % 2 == 0:
-          self._score_team_1 += score
-          if self._training_data is not None:
-            training_data_team_1.append(np.append(states[0],  score * HAND_SCORE_FACTOR))
-            training_data_team_2.append(np.append(states[1], -score * HAND_SCORE_FACTOR))
-            training_data_team_1.append(np.append(states[2],  score * HAND_SCORE_FACTOR))
-            training_data_team_2.append(np.append(states[3], -score * HAND_SCORE_FACTOR))
-        else:
-          self._score_team_2 += score
-          if self._training_data is not None:
-            training_data_team_1.append(np.append(states[0], -score * HAND_SCORE_FACTOR))
-            training_data_team_2.append(np.append(states[1],  score * HAND_SCORE_FACTOR))
-            training_data_team_1.append(np.append(states[2], -score * HAND_SCORE_FACTOR))
-            training_data_team_2.append(np.append(states[3],  score * HAND_SCORE_FACTOR))
 
+      # update score
+      if dealer % 2 == 0:
+        self._score_team_1 += score
+      else:
+        self._score_team_2 += score
+
+      # if we gather training data and actually had a decision to make, update training data
+      if i < 8 and self._training_data is not None:
+        self._update_current_training_data(training_data_team_1, training_data_team_2, states, score, dealer)
+
+    # the hand is done, add 5 points for the last stich
     if dealer % 2 == 0:
       self.log.info("Team 1 made the last stich")
       self._score_team_1 += 5
@@ -64,6 +67,7 @@ class Hand():
       self.log.info("Team 2 made the last stich")
       self._score_team_2 += 5
 
+    # after concluding the round, update the global training data
     if self._training_data is not None:
       for i in range(len(training_data_team_1)):
         training_data_team_1[i][-1] += self._score_team_1
@@ -71,6 +75,18 @@ class Hand():
         training_data_team_2[i][-1] += self._score_team_2
       self._training_data.extend(training_data_team_1)
       self._training_data.extend(training_data_team_2)
+
+  def _update_current_training_data(self, training_data_team_1, training_data_team_2, states, score, dealer):
+    if dealer % 2 == 0:
+      training_data_team_1.append(np.append(states[0],  score * Hand.HAND_SCORE_FACTOR))
+      training_data_team_2.append(np.append(states[1], -score * Hand.HAND_SCORE_FACTOR))
+      training_data_team_1.append(np.append(states[2],  score * Hand.HAND_SCORE_FACTOR))
+      training_data_team_2.append(np.append(states[3], -score * Hand.HAND_SCORE_FACTOR))
+    else:
+      training_data_team_1.append(np.append(states[0], -score * Hand.HAND_SCORE_FACTOR))
+      training_data_team_2.append(np.append(states[1],  score * Hand.HAND_SCORE_FACTOR))
+      training_data_team_1.append(np.append(states[2], -score * Hand.HAND_SCORE_FACTOR))
+      training_data_team_2.append(np.append(states[3],  score * Hand.HAND_SCORE_FACTOR))
 
   @property
   def result(self):
--- round.py
+++ round.py
@@ -15,10 +15,22 @@ class Round():
     self.log = log
 
   def play(self, dealer):
+    """Play a round where each player plays one card.
+
+    :dealer: (int) Index of the player that plays the first card.
+
+    :returns: A tuple consisting of:
+    - Index of the player who won the round
+    - The score of the round
+    - An array with the played cards (in order of player, not in order of played card)
+    - The player states during this round
+    """
     # self._print_hands()
 
     played_cards = []
     states = []
+
+    # play round
     for i in range(4):
       current_player = self._players[(dealer+i) % 4]
       played_card, player_state = current_player.select_card_to_play(played_cards, self._known_cards)
@@ -27,19 +39,22 @@ class Round():
       played_cards.append(played_card)
       states.append(player_state)
 
+    # evaluate round
     winner, score = self._evaluate(played_cards, dealer)
     self.log.info("{} wins the round ({} points)".format(self._players[winner].name, score))
-    return winner, np.roll(played_cards, -dealer), score, np.roll(states, -dealer)
+    return winner, score, np.roll(played_cards, -dealer), np.roll(states, -dealer)
 
   def _evaluate(self, played_cards, dealer):
+    # find the index of the best card among the played cards
     best_index = 0
     for i in range(1, 4):
       if played_cards[best_index].is_beaten_by(played_cards[i]):
         best_index = i
+
+    # the winner is the best card offset by the initial dealer
     winner = (dealer + best_index) % 4
 
     score = sum(map(lambda c: c.score, played_cards))
-
     return winner, score
 
   def _print_hands(self):
