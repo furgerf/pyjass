20defbbb5c63918b7d8323e2564066a1741c6a59
Fabian Furger <furgerfabian@hotmail.com>
2018-03-17 20:28:28 +0100

    Add loss curve WIP

--- src/explore.py
+++ src/explore.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # pylint: disable=unused-import
+import os
 import csv
 import matplotlib.pyplot as plt
 from matplotlib.ticker import EngFormatter
 import numpy as np
 import pandas as pd
-# import seaborn as sns
 import pickle
 
 import utils
@@ -24,48 +24,47 @@ def store_model(model, file_name):
     return pickle.dump(model, fh)
 
 
-def visualize_scores(eid="foo", ymin=0.0, ymax=1.0):
+def visualize_scores(eid):
   scores_file = "evaluations/{}/curve_scores.csv".format(eid)
-  df = pd.read_csv(scores_file)
+  loss_file = "evaluations/{}/loss.csv".format(eid)
 
-  # prepare data
-  df["win_percentage"] = df.wins_team_1 / (df.wins_team_1 + df.wins_team_2)
-  df["score_percentage"] = df.score_team_1 / (df.score_team_1 + df.score_team_2)
+  scores = pd.read_csv(scores_file) if os.path.exists(loss_file) else None
+  loss = pd.read_csv(loss_file) if os.path.exists(loss_file) else None
 
-  df["cum_wins_team_1"] = df.wins_team_1.cumsum()
-  df["cum_wins_team_2"] = df.wins_team_2.cumsum()
-  df["cum_score_team_1"] = df.score_team_1.cumsum()
-  df["cum_score_team_2"] = df.score_team_2.cumsum()
-  df["cum_win_percentage"] = df.cum_wins_team_1 / (df.cum_wins_team_1 + df.cum_wins_team_2)
-  df["cum_score_percentage"] = df.cum_score_team_1 / (df.cum_score_team_1 + df.cum_score_team_2)
 
   # plot
   fig = plt.figure()
-  ax = fig.add_subplot(111)
-  ax.xaxis.set_major_formatter(EngFormatter())
-  ax.yaxis.set_major_locator(plt.MultipleLocator(0.05))
-  ax.minorticks_on()
-  plt.title("{}: Team 1 win/score percentage".format(eid))
-  plt.ylim(ymin, ymax)
   plt.grid()
-  plt.tight_layout()
 
-  plt.plot(df.hand, df.win_percentage, "k.-", c="g", alpha=0.3, label="Team 1 win %")
-  plt.plot(df.hand, df.score_percentage, "k.-", c="b", alpha=0.3, label="Team 1 score %")
-  plt.plot(df.hand, df.cum_win_percentage, "k.-", c="g", label="Cum team 1 win %")
-  plt.plot(df.hand, df.cum_score_percentage, "k.-", c="b", label="Cum team 1 score %")
+  score_ax = fig.add_subplot(111)
+  score_ax.xaxis.set_major_formatter(EngFormatter())
+  score_ax.yaxis.set_major_locator(plt.MultipleLocator(0.05))
+  score_ax.minorticks_on()
+  score_ax.set_ylim(0, 1)
 
-  plt.axhline(y=0.5, c="r", alpha=0.3, linestyle="--")
+  if scores is not None:
+    scores["win_percentage"] = scores.wins_team_1 / (scores.wins_team_1 + scores.wins_team_2)
+    scores["score_percentage"] = scores.score_team_1 / (scores.score_team_1 + scores.score_team_2)
+    score_ax.plot(scores.team_1_info, scores.win_percentage, "k.-", c="g", label="Team 1 win %")
+    score_ax.plot(scores.team_1_info, scores.score_percentage, "k.-", c="b", label="Team 1 score %")
 
-  if "team_1_info" in df.columns and "team_2_info" in df.columns:
-    for i, index in enumerate(df.team_1_info.diff()[df.team_1_info.diff() != 0].index.values):
+  if loss is not None:
+    loss_ax = score_ax.twinx()
+    loss_ax.set_ylim(0, 1000)
+
+    loss_ax.plot(loss.samples, loss.loss, "k.-", c="r", label="Model loss")
+
+  score_ax.axhline(y=0.5, c="r", alpha=0.3, linestyle="--")
+
+  if scores is not None and "team_1_info" in scores.columns and "team_2_info" in scores.columns:
+    for i, index in enumerate(scores.team_1_info.diff()[scores.team_1_info.diff() != 0].index.values):
       # NOTE: both teams have the same training interval
-      row = df.iloc[index]
+      row = scores.iloc[index]
 
       if pd.isnull(row.team_1_info) and pd.isnull(row.team_2_info):
         continue
 
-      plt.axvline(x=row.hand, c="purple", alpha=0.5, linestyle="-")
+      plt.axvline(x=row.team_1_info, c="purple", alpha=0.5, linestyle="-")
       if not pd.isnull(row.team_1_info) and not pd.isnull(row.team_2_info):
         note = "T1:{}\nT2:{}".format(utils.format_human(row.team_1_info), utils.format_human(row.team_2_info))
       elif not pd.isnull(row.team_1_info):
@@ -73,10 +72,15 @@ def visualize_scores(eid="foo", ymin=0.0, ymax=1.0):
       elif not pd.isnull(row.team_2_info):
         note = "T2:{}".format(utils.format_human(row.team_2_info))
 
-      plt.annotate(note, color="purple",
-          xy=(row.hand, plt.ylim()[0] + 0.01 + 0.02*(i%4)), xytext=(row.hand, plt.ylim()[0] + 0.01 + 0.02*(i%4)))
+      score_ax.annotate(note, color="purple",
+          xy=(row.team_1_info, plt.ylim()[0] + 0.01 + 0.02*(i%4)), xytext=(row.team_1_info, plt.ylim()[0] + 0.01 + 0.02*(i%4)))
 
-  plt.legend()
+  if loss is not None:
+    loss_ax.legend()
+  score_ax.legend()
+
+  plt.title("{}: Team 1 win/score percentage".format(eid))
+  plt.tight_layout()
   plt.show()
 
-  return df
+  return scores, loss
